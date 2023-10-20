2ba1c21602ef139167183b6293a14271aeeef275
Kurt McKee <contactme@kurtmckee.org>
2023-10-20 07:14:54 -0500

    Refactor `tests/test_exceptions.py`

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,34 +1,32 @@
 import pytest
 
-from jsonpath_ng import parse as rw_parse
-from jsonpath_ng.exceptions import JSONPathError, JsonPathParserError
+from jsonpath_ng import parse as base_parse
+from jsonpath_ng.exceptions import JsonPathParserError
 from jsonpath_ng.ext import parse as ext_parse
 
 
 @pytest.mark.parametrize(
     "path",
     (
-        'foo[*.bar.baz',
-        'foo.bar.`grandparent`.baz',
-        # error at the end of string
-        'foo[*',
+        "foo[*.bar.baz",
+        "foo.bar.`grandparent`.baz",
+        "foo[*",
         # `len` extension not available in the base parser
-        'foo.bar.`len`',
-    )
+        "foo.bar.`len`",
+    ),
 )
 def test_rw_exception_subclass(path):
     with pytest.raises(JsonPathParserError):
-        rw_parse(path)
+        base_parse(path)
 
 
 @pytest.mark.parametrize(
     "path",
     (
-        'foo[*.bar.baz',
-        'foo.bar.`grandparent`.baz',
-        # error at the end of string
-        'foo[*',
-    )
+        "foo[*.bar.baz",
+        "foo.bar.`grandparent`.baz",
+        "foo[*",
+    ),
 )
 def test_ext_exception_subclass(path):
     with pytest.raises(JsonPathParserError):
